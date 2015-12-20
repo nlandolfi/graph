f19b3f5cd09eb8e1f84df018494efd91564db1f7
Nick Landolfi <nclandolfi@gmail.com>
2015-12-19 23:14:45 -0800

    add breadth first benchmark

--- search_test.go
+++ search_test.go
@@ -30,3 +30,27 @@ func BenchmarkDFS(b *testing.B) {
 
 	llist = l
 }
+
+func BenchmarkBFS(b *testing.B) {
+	// run the Fib function b.N times
+	S := NewNode(1)
+	A := NewNode(2)
+	B := NewNode(3)
+	C := NewNode(4)
+	G := NewNode(5)
+
+	S.edges = &[]*node{A, B} // note b wins tiebreak
+	A.edges = &[]*node{B, C} // note c wins tiebreak
+	B.edges = &[]*node{C}
+	C.edges = &[]*node{G}
+
+	var l *list.List
+
+	for n := 0; n < b.N; n++ {
+		l, _ = BreadthFirstSearch(S, func(n *node) bool {
+			return n == G
+		})
+	}
+
+	llist = l
+}
